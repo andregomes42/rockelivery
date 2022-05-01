dbc2607ad8e117ec6b99ca09b8b331f945e1ee7b
andregomes42 <andregomesngr@gmail.com>
2022-05-01 15:14:44 -0500

    initial commit

---
+++ config/test.exs
@@ -0,0 +1,24 @@
+use Mix.Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :rockelivery, Rockelivery.Repo,
+  username: "inciclemaster",
+  password: "incicle-pass",
+  database: "postgres",
+  hostname: "localhost",
+  port: "55432",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :rockelivery, RockeliveryWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
